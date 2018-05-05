87af45d91fbfb5907962dda03532081b202650d0
Crystal O <crystalodi@gmail.com>
2018-05-04 20:16:10 -0500

    add callback fn to update in burger models. add burger block template

--- models/burger.js
+++ models/burger.js
@@ -4,16 +4,18 @@ var burger = {
   all: function(cb) {
     orm.selectAll("burgers", function(res){
       cb(res);
-    })
+    });
   },
   add: function(cols, vals, cb) {
     orm.insertOne("burgers", cols, vals, function(res){
       cb(res);
-    })
+    });
 
   },
   update: function(objColVals, condition, cb) {
-    orm.updateOne("burgers", objColVals)
+    orm.updateOne("burgers", objColVals, function(res){
+      cb(res);
+    });
   }
 }
 
