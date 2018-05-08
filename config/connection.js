616f2479cbd94654862321d71b2d83e7ea9d220e
Crystal O <crystalodi@gmail.com>
2018-05-07 20:31:20 -0500

    add jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  user: "root",
-  password: "WowNoU89!",
-  host: "localhost",
-  database: "burgers_db"
-});
-connection.connect(function(err){
-  if(err) {
-    return console.error("An error occured: " + err.stack);
-  }
-  console.log("Connected on " + connection.threadId);
-})
+
+var connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    user: "root",
+    password: "WowNoU89!",
+    host: "localhost",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 module.exports = connection;
